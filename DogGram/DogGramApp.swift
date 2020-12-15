d6f1087b70f53d9a9554d3431a8bebcecf961ea1
Yaqi <wangyaqi9506@gmail.com>
2020-12-14 21:07:02 -0800

    project

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by Yaqi Wang on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
